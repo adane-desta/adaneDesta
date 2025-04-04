c49d4d71b6bfe6e00e338e4cd2ada77b4f98e403
adane-desta <addonegn@gmail.com>
2025-04-04 11:51:41 +0300

    server-commited-again

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import router from './routes/routes.js';
+import db from '../config/db.js'
 
 dotenv.config();
 const app = express();
@@ -21,9 +22,26 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use('/api', router);
 
-app.get('/render' , (req , res) => {
+app.get('/render' , async (req , res) => {
 
-    res.send("wellcome render")
+    const connection = await db.getConnection();
+      await connection.beginTransaction();
+
+      try{
+
+        const [userQuestionData] = await connection.query(
+            `SELECT question_id, name , emailorphone , question_text ,  address FROM userquestions`);
+            res.status(200).json({ data: userQuestionData });
+           
+      }catch (error){
+
+       await connection.rollback();
+       throw error;
+
+      } finally {
+        connection.release();
+    }
+
 
 })
 app.listen(PORT, () => {
